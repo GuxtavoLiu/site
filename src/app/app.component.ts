69afd52ff628b5f4138ae4ca50c432b74ba9d15a
GustavoLiu <gustavo_santostr@hotmail.com>
2019-02-28 22:38:21 -0300

    Calculadora em Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent{
+  nome: string = "Liu";
+  valor: string = "";
+
+  n1: string = "";
+  n2: string = "";
+
+
+
+
+  calcular( n1: number, n2: number) : number{
+  resultado: number = Number(this.n1) + Number(this.n2);
+  return resultado;
+  }
+
+
+  clique() : void{
+    alert(this.valor);
+  }
+
 }
